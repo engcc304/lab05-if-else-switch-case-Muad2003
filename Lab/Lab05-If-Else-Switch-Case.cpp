067c84cf3c3b4de4487b6a59d715fb61dce281b0
Muad2003 <anuphap2003118@gmail.com>
2023-07-27 14:28:49 +0700

    update lap5

--- Lab/Lab05-If-Else-Switch-Case.cpp
+++ Lab/Lab05-If-Else-Switch-Case.cpp
@@ -37,29 +37,28 @@
 #include <cctype>
 int main() {
 
-    //--| YOUR CODE HERE
     int score ;
-    printf( "Enter scrore : " ) ;
+    printf( "enter scrore : " ) ;
     if( scanf( "%d" ,&score ) ==1 ) {
         if( score >= 80 ) {
-        printf( "A" ) ;
+        printf( "A !" ) ;
     } else if ( score >= 75 && score <= 79 ){
-        printf( "B+" ) ;
+        printf( "B+ !" ) ;
     } else if ( score >= 70 && score <= 74 ){
-        printf( "B" ) ;
+        printf( "B !" ) ;
     } else if ( score >= 65 && score <= 69 ){
-        printf( "C+" ) ;
+        printf( "C+ !" ) ;
     } else if ( score >= 60 && score <= 64 ){
-        printf( "C" ) ;
+        printf( "C !" ) ;
     } else if ( score >= 55 && score <= 59 ){
-        printf( "D+" ) ;
+        printf( "D+ !" ) ;
     } else if ( score >= 50 && score <= 54 ){
-        printf( "D" ) ;
+        printf( "D !" ) ;
     } else if ( score <= 49 ){
-        printf( "F" ) ;
+        printf( "F !" ) ;
     }
     } else {
-        printf( "Numble Only" ) ;
+        printf( "Please Enter Number" ) ;
     }
     return 0 ;
 }//end main function
