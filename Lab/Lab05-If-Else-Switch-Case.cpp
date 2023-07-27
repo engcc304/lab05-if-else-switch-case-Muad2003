74dc2dd67bc96d71e376ff268db1fe0debaf9a53
Muad2003 <anuphap2003118@gmail.com>
2023-07-27 13:37:50 +0700

    update lap5

--- Lab/Lab05-If-Else-Switch-Case.cpp
+++ Lab/Lab05-If-Else-Switch-Case.cpp
@@ -38,6 +38,27 @@
 int main() {
 
     //--| YOUR CODE HERE
+    int score ;
+    printf( "Enter scrore : " ) ;
+    scanf( "%d" ,&score ) ;
 
+    if( score >= 80 ) {
+        printf( "A" ) ;
+    } else if ( score >= 75 && score <= 79 ){
+        printf( "B+" ) ;
+    } else if ( score >= 70 && score <= 74 ){
+        printf( "B" ) ;
+    } else if ( score >= 65 && score <= 69 ){
+        printf( "C+" ) ;
+    } else if ( score >= 60 && score <= 64 ){
+        printf( "C" ) ;
+    } else if ( score >= 55 && score <= 59 ){
+        printf( "D+" ) ;
+    } else if ( score >= 50 && score <= 54 ){
+        printf( "D" ) ;
+    } else if ( score <= 49 ){
+        printf( "F" ) ;
+    } 
+    
     return 0 ;
 }//end main function
